6c2fe66fea55d033d0a321a5f2d003addee2965b
yisenlin <ysl.130@qq.com>
2023-01-27 21:28:23 +0800

    update i_val

--- load_avt.py
+++ load_avt.py
@@ -15,17 +15,13 @@ def gen_split(imgs, testskip):
 
     for i in range(len(imgs)):
         b = random.randint(0,9)
-        if b >=0 and b<8:
+        if b >=0 and b<9:
             i_train.append(i)
-        elif b>=8 and b<9:
-            i_val.append(i)
         else:
             i_test.append(i)
 
     if len(i_train) == 0:
         i_train.append(random.randint(0, len(imgs)-1))  
-    if len(i_val) == 0:
-        i_val.append(random.randint(0, len(imgs)-1))
     if len(i_test) == 0:
         i_test.append(random.randint(0, len(imgs)-1))
 
@@ -34,15 +30,15 @@ def gen_split(imgs, testskip):
     else:
         skip = testskip
 
-    i_val = i_val[::skip]
     i_test = i_test[::skip]
+    i_val = i_test
 
     i_train = sorted(i_train)
     i_val = sorted(i_val)
     i_test = sorted(i_test)
     return [i_train, i_val, i_test]
 
-def load_avt_data(basedir, half_res=False, testskip=1):
+def load_avt_data(basedir, half_res=False, testskip=1, need_fix=True):
     with open(os.path.join(basedir, 'transforms.json'), 'r') as fp:
         meta = json.load(fp)
 
@@ -57,11 +53,15 @@ def load_avt_data(basedir, half_res=False, testskip=1):
             file_path = file_path + '.png'
         fname = os.path.join(basedir, file_path)
         imgs.append(imageio.imread(fname))
-        T_cam_face_to_world = np.array(frame['transform_matrix'])
-        #fix poses
-        T_img_to_cam_face = np.eye(4) 
-        T_img_to_cam_face[:3, :3] = R.from_euler("xyz", [180, 0, 0], degrees=True).as_matrix()
-        T_cam_to_world = T_cam_face_to_world @ T_img_to_cam_face
+        T_cam_to_world = np.array(frame['transform_matrix'])
+        if need_fix:
+            #fix poses
+            T_cam_face_to_world = T_cam_to_world
+
+            T_img_to_cam_face = np.eye(4) 
+            T_img_to_cam_face[:3, :3] = R.from_euler("xyz", [180, 0, 0], degrees=True).as_matrix()
+            T_cam_to_world = T_cam_face_to_world @ T_img_to_cam_face
+
         poses.append(T_cam_to_world)
     imgs = (np.array(imgs) / 255.).astype(np.float32)
     poses = np.array(poses).astype(np.float32)
@@ -72,6 +72,7 @@ def load_avt_data(basedir, half_res=False, testskip=1):
     poses = np.concatenate(all_poses, 0)
     
     H, W = imgs[0].shape[:2]
+    # angle error in avt data
     # camera_angle_x = float(meta['camera_angle_x'])
     # focal = .5 * W / np.tan(.5 * camera_angle_x)
     focal = float(meta['fx'])
@@ -98,11 +99,12 @@ def load_avt_data(basedir, half_res=False, testskip=1):
         [0, fy, cy],
         [0, 0, 1]
     ])
-    # K = None
-        
 
     render_poses = torch.stack([pose_spherical(angle, -30.0, 4.0) for angle in np.linspace(-180,180,40+1)[:-1]], 0)
 
     i_split = gen_split(imgs, testskip)
 
     return imgs, poses, render_poses, [H, W, focal], i_split, K
+
+def load_avt_data_v2(basedir, half_res=False, testskip=1):
+    return load_avt_data(basedir, half_res, testskip, False)
